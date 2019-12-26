bfe4c46c4d2d03c8f34d341d7968df8c125e43bc
Maria Bilous <maria.bilous101@gmail.com>
2019-12-26 14:08:37 +0200

    practicing @Qalifier

--- src/main/java/ua/java_training/MusicPlayer.java
+++ src/main/java/ua/java_training/MusicPlayer.java
@@ -1,21 +1,23 @@
 package ua.java_training;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
-    @Autowired // here two dependencies will be injected accordingly
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    @Autowired // here two dependencies will be injected according to the annotation @ Qualifier
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 
 //    private List<Music> music = new ArrayList<>();
