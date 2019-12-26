df9a2b34cbc0a59da6404fd01b9e8e73e731ff46
Maria Bilous <maria.bilous101@gmail.com>
2019-12-26 10:40:43 +0200

    init(), destroy(), factory methods applied

--- src/main/java/ua/java_training/ClassicalMusic.java
+++ src/main/java/ua/java_training/ClassicalMusic.java
@@ -1,6 +1,22 @@
 package ua.java_training;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        System.out.println("Classical music instantiating...");
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMydestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rapsody";
