5c42087d0993224e56b8851cc36cb0d424fdece0
Maria Bilous <maria.bilous101@gmail.com>
2019-12-26 18:32:16 +0200

    simple mvc project

--- src/main/java/ua/java_training/HelloController.java
+++ src/main/java/ua/java_training/HelloController.java
@@ -1,4 +1,13 @@
 package ua.java_training;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/hello-my-big-world")
+    public String sayHello() {
+        return "hello_my_big_world"; // this is owr view name
+    }
 }
